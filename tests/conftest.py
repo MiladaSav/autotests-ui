efa3da3049fd70a208fa38e904c434ddf4bc389e
Milada <savitskaya.m@bookcentre.ru>
2025-10-18 21:43:30 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,16 @@ import pytest
 from playwright.sync_api import Playwright, Page
 
 
+@pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
+def chromium_page(playwright: Playwright) -> Page:  # Аннотируем возвращаемое фикстурой значение
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    yield page
+
+    browser.close()
+
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -35,3 +45,4 @@ def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -
     page = context.new_page()
 
     yield page
+
